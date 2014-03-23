3c1835b67897a9f3e6ccf0c370809144e4883577
Robert Hintz <roberth@surveymonkey.com>
2014-03-23 01:32:14 -0700

    Modify RowProxy Object to act more like actual RowProxy

--- row_proxy.py
+++ row_proxy.py
@@ -2,17 +2,40 @@
 
 class RowProxy(object):
     def __init__(self, *args, **kwargs):
-        self.row = ()
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
-            setattr(self, key.lower(), kwargs[key])
-            setattr(self, key.upper(), kwargs[key])
-        #Add each val in kwargs to self.row tuple
+        self._row = ()
+        self._keys = []
+        self._values = []
+        self._dict = {}
         for key,val in kwargs.items():
-            self.row = self.row + (val,)
+            setattr(self, key, val)
+            setattr(self, key.lower(), val)
+            setattr(self, key.upper(), val)
+            self._keys.append(key)
+            self._row = self._row + (val,)
+            self._values.append(val)
+            self._dict[key] = val
 
     def __repr__(self):
-        return str(self.row)
+        return str(self._row)
 
     def __str__(self):
-        return str(self.row)
+        return str(self._row)
+
+    def __getitem__(self, attr):
+        return self._dict[attr]
+
+    def __getattr__(self, attr):
+        self._raise_column_error(attr)
+
+    def keys(self):
+        return self._keys
+
+    def values(self):
+        return self._values
+
+    @property
+    def __dict__(self):
+        self._raise_column_error('dict')
+
+    def _raise_column_error(self, attr):
+        raise AttributeError("Could not locate column in row for column '%s'" % (attr))
